e790a50a567e75d1294d533a203d3852a3996bc7
blancopado <pablo.dominguezblanco@gmail.com>
2015-12-02 17:56:52 +0100

    interacting with elements

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -4,4 +4,14 @@ describe('Github Profile Finder', function() {
 
 		expect(browser.getTitle()).toEqual('Github user search');
 	});
+
+	it('finds profiles', function() {
+    browser.get('http://localhost:8080');
+
+    element(by.model('searchTerm')).sendKeys('spike01');
+    element(by.className('btn')).click();
+
+    expect(element(by.binding('user.login')).getText()).
+        toEqual('spike0');
+  });
 });
